437337c1fb7118f27128cc99e99b4b0b9adffa03
Sven-Hendrik Haase <svenstaro@gmail.com>
2023-12-03 12:21:13 +0100

    Nicer map

--- data/background_tiles.tsx
+++ data/background_tiles.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="background_tiles" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="MasterSimple.png" width="256" height="256"/>
+ <wangsets>
+  <wangset name="Paths" type="edge" tile="-1">
+   <wangcolor name="Path" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="12" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="13" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="14" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="15" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="28" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="29" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="30" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="31" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="44" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="45" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="46" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="47" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="61" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="62" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="63" wangid="0,0,1,0,1,0,1,0"/>
+  </wangset>
+ </wangsets>
 </tileset>
--- src/main.rs
+++ src/main.rs
@@ -31,44 +31,153 @@ impl tiled::ResourceReader for TiledReader {
 }
 
 fn draw_background(world_map: &Map, tileset: &Tileset, texture: &Texture2D) {
-    let background_layer = world_map
-        .layers()
-        .find(|l| l.name == "background")
-        .unwrap()
-        .as_tile_layer()
-        .unwrap();
-    for x in 0..background_layer.width().unwrap() {
-        for y in 0..background_layer.height().unwrap() {
-            if let Some(tile) = background_layer.get_tile(x as i32, y as i32) {
-                let tile_id = tile.id();
-                let tile_width = tileset.tile_width;
-                let tile_height = tileset.tile_height;
-                let spacing = tileset.spacing;
-                let margin = tileset.margin;
-                let tiles_per_row =
-                    (texture.size().x as u32 - margin + spacing) / (tile_width + spacing);
-                let tileset_texture_x = tile_id % tiles_per_row * tile_width;
-                let tileset_texture_y = tile_id / tiles_per_row * tile_height;
+    for layer in world_map.layers() {
+        let layer = layer.as_tile_layer().unwrap();
+        for x in 0..layer.width().unwrap() {
+            for y in 0..layer.height().unwrap() {
+                if let Some(tile) = layer.get_tile(x as i32, y as i32) {
+                    let tile_id = tile.id();
+                    let tile_width = tileset.tile_width;
+                    let tile_height = tileset.tile_height;
+                    let spacing = tileset.spacing;
+                    let margin = tileset.margin;
+                    let tiles_per_row =
+                        (texture.size().x as u32 - margin + spacing) / (tile_width + spacing);
+                    let tileset_texture_x = tile_id % tiles_per_row * tile_width;
+                    let tileset_texture_y = tile_id / tiles_per_row * tile_height;
 
-                draw_texture_ex(
-                    &texture,
-                    (x * tile_width) as f32,
-                    (y * tile_height) as f32,
-                    WHITE,
-                    DrawTextureParams {
-                        flip_x: tile.flip_h,
-                        flip_y: tile.flip_v,
-                        source: Some(Rect::new(
-                            tileset_texture_x as f32,
-                            tileset_texture_y as f32,
-                            tile_width as f32,
-                            tile_height as f32,
-                        )),
-                        ..Default::default()
-                    },
-                )
+                    draw_texture_ex(
+                        &texture,
+                        (x * tile_width) as f32,
+                        (y * tile_height) as f32,
+                        WHITE,
+                        DrawTextureParams {
+                            flip_x: tile.flip_h,
+                            flip_y: tile.flip_v,
+                            source: Some(Rect::new(
+                                tileset_texture_x as f32,
+                                tileset_texture_y as f32,
+                                tile_width as f32,
+                                tile_height as f32,
+                            )),
+                            ..Default::default()
+                        },
+                    )
+                }
             }
         }
+    }
+}
+
+pub struct Character {
+    pub name: String,
+    pub job: String,
+    pub position: Vec2,
+    pub idle_sprite: AnimatedSprite,
+    pub idle_texture: Texture2D,
+    pub walk_sprite: AnimatedSprite,
+    pub walk_texture: Texture2D,
+}
+
+impl Character {
+    async fn new(
+        name: &str,
+        job: &str,
+        position: Vec2,
+        idle_animation: &str,
+        walk_animation: &str,
+    ) -> Result<Character> {
+        let idle_sprite = AnimatedSprite::new(
+            16,
+            16,
+            &[
+                Animation {
+                    name: "idle_down".to_string(),
+                    row: 0,
+                    frames: 4,
+                    fps: 5,
+                },
+                Animation {
+                    name: "idle_left".to_string(),
+                    row: 1,
+                    frames: 4,
+                    fps: 5,
+                },
+                Animation {
+                    name: "idle_up".to_string(),
+                    row: 2,
+                    frames: 4,
+                    fps: 5,
+                },
+                Animation {
+                    name: "idle_right".to_string(),
+                    row: 3,
+                    frames: 4,
+                    fps: 5,
+                },
+            ],
+            true,
+        );
+        let idle_texture = load_texture(idle_animation).await?;
+
+        let walk_sprite = AnimatedSprite::new(
+            16,
+            16,
+            &[
+                Animation {
+                    name: "walk_down".to_string(),
+                    row: 0,
+                    frames: 4,
+                    fps: 5,
+                },
+                Animation {
+                    name: "walk_left".to_string(),
+                    row: 1,
+                    frames: 4,
+                    fps: 5,
+                },
+                Animation {
+                    name: "walk_up".to_string(),
+                    row: 2,
+                    frames: 4,
+                    fps: 5,
+                },
+                Animation {
+                    name: "walk_right".to_string(),
+                    row: 3,
+                    frames: 4,
+                    fps: 5,
+                },
+            ],
+            true,
+        );
+        let walk_texture = load_texture(walk_animation).await?;
+
+        Ok(Character {
+            name: name.to_string(),
+            job: job.to_string(),
+            position,
+            idle_sprite,
+            idle_texture,
+            walk_sprite,
+            walk_texture,
+        })
+    }
+
+    pub fn draw(&mut self) {
+        draw_texture_ex(
+            &self.idle_texture,
+            self.position.x,
+            self.position.y,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(self.idle_sprite.frame().dest_size),
+                source: Some(self.idle_sprite.frame().source_rect),
+                ..Default::default()
+            },
+        );
+
+        self.idle_sprite.update();
     }
 }
 
@@ -84,19 +193,6 @@ async fn main() -> Result<()> {
     let background_texture = load_texture("data/MasterSimple.png").await.unwrap();
     background_texture.set_filter(FilterMode::Nearest);
 
-    let mut char1_idle_sprite = AnimatedSprite::new(
-        16,
-        16,
-        &[Animation {
-            name: "idle".to_string(),
-            row: 0,
-            frames: 4,
-            fps: 5,
-        }],
-        true,
-    );
-    let char1_idle_texture = load_texture("data/char1_idle.png").await?;
-
     // We want to be able to resize the window in such a way that the contents are always
     // aspect-preserved while always getting scaled in the best possible way.
     let map_width = (world_map.width * world_map.tile_width) as f32;
@@ -108,25 +204,42 @@ async fn main() -> Result<()> {
         Camera2D::from_display_rect(Rect::new(0.0, 0.0, map_width, map_height));
     render_target_camera.render_target = Some(render_target.clone());
 
+    let mut characters = vec![
+        Character::new(
+            "Kas",
+            "Shopkeeper",
+            vec2(100.0, 100.0),
+            "data/char1_idle.png",
+            "data/char1_walk.png",
+        )
+        .await?,
+        Character::new(
+            "Jeid",
+            "Barkeeper",
+            vec2(200.0, 200.0),
+            "data/char2_idle.png",
+            "data/char2_walk.png",
+        )
+        .await?,
+        Character::new(
+            "Bres",
+            "Peasant",
+            vec2(400.0, 230.0),
+            "data/char3_idle.png",
+            "data/char3_walk.png",
+        )
+        .await?,
+    ];
+
     loop {
         set_camera(&render_target_camera);
 
         draw_background(&world_map, &background_tileset, &background_texture);
 
         // Draw characters.
-        draw_texture_ex(
-            &char1_idle_texture,
-            100.0,
-            100.0,
-            WHITE,
-            DrawTextureParams {
-                dest_size: Some(char1_idle_sprite.frame().dest_size),
-                source: Some(char1_idle_sprite.frame().source_rect),
-                ..Default::default()
-            },
-        );
-
-        char1_idle_sprite.update();
+        for character in &mut characters {
+            character.draw();
+        }
 
         // Draw FPS.
         draw_text(format!("FPS: {}", get_fps()).as_str(), 8., 16., 16., WHITE);
